5596613c36c2a9d02f74bee2b6fa4037531c104e
ethanwads <eswadswo@ucsd.edu>
2020-10-02 12:37:53 -0700

    backend files for firestore and auth config

---
+++ auth.js
@@ -0,0 +1,67 @@
+// might split file up, as login and signup are on separate pages
+// still need to allow users to add ingredients 
+
+// getting data for the specific user if they are logged in
+let user = firebase.auth().currentUser
+db.collection('users').get()
+.then(snapshot => {
+    // method to render current ingredients list for the user logged in
+    setupIngredientsList(snapshot.docs)
+})
+
+// listen for auth changes
+// useful for conditional rendering and restricting data the user sees
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user logged in', user)
+    } else {
+        console.log('user logged out')
+    }
+})
+
+// signing up
+const signupForm = document.querySelector('.signupForm')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //get user info
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(credential => {
+        const userId = credential.user.uid 
+        const userData = {
+            ingredients: []
+        }
+        db.collection("users").doc(userId).set(userData).then(() => {
+            console.log("User successfully added to the DB!");
+          })
+        .catch((e) => {
+            console.log("Error adding user to the DB: ", e);
+        });
+    })
+})
+
+// logout
+const logout = document.querySelector('.logout')
+logout.addEventListener('click', (e) => {
+    e.preventDefault()
+    auth.signOut()
+    .then(() => {
+        console.log('user signed out')
+    })
+})
+
+//login
+const loginForm = document.querySelector('id/class')
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = signupForm['email'].value
+    const password = signupForm['password'].value
+    auth.signInWithEmailAndPassword(email, password)
+    .then(cred => {
+        console.log(cred)
+    })
+})
